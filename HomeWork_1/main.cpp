34710719207c36b33540a3b56f22d9131cb67a68
mak-p <mak-p@DESKTOP-PG1IP70>
2022-07-18 21:20:58 +0300

    working code

--- HomeWork_1/main.cpp
+++ HomeWork_1/main.cpp
@@ -2,21 +2,35 @@
 using namespace std;
 
 int Factorial(int n);
-
+double Power(int a, int b);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
-	cout << "Введите число для вычесления Факторила: "; cin >> n;
-	cout << n << " ! " << " = " << Factorial(n) << endl;
-	int factorial(int n);
-	{
-		int a = 1;
-		for (i = 1; i <= n; i++)
-		{
-			a *= i;
-			return a;
-		}
-	}
+	cout << "Введите чило для вычесления факториала: "; cin >> n;
+	cout << n << " = " << Factorial(n) << endl;
+	int a, b;
+	cout << "Возведение в степень "<<endl;
+	cout << "Введите число: "; cin >> a;
+	cout << "Введите степень числа: "; cin >> b;
+	cout << a << " ^ " << b <<" = "<< Power(a, b) << endl;
 }
+	int Factorial(int n)
+	{
+		int f = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			f *= i;
+		}
+		return f;
+	}
+	double Power(int a, int b)
+	{
+		int N = 1;
+		for (int i = 0; i < b; i++)
+		{
+			N *= a;
+		}
+		return N;
+	}
