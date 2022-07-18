6eb804af63f67e0c81bc04f7b650920b74a2867a
mak-p <mak-p@DESKTOP-PG1IP70>
2022-07-18 09:18:09 +0300

    home work

---
+++ HomeWork_1/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int Factorial(int n);
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите число для вычесления Факторила: "; cin >> n;
+	cout << n << " ! " << " = " << Factorial(n) << endl;
+	int factorial(int n);
+	{
+		int a = 1;
+		for (i = 1; i <= n; i++)
+		{
+			a *= i;
+			return a;
+		}
+	}
+}
